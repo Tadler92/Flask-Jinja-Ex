a4c73688bfd34fc3abb82d80113fc58f688a9a98
Trevor Duhon <taduhon1485@gmail.com>
2024-01-25 16:35:46 -0600

    Initialized git repo for 'Jinja' madlib exercise, as well as started code problems

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from random import randint, choice, sample
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def show_homepage():
+    """Brings us to the homepage of the application"""
+    prompts = story.prompts
+    return render_template('home.html', prompts=prompts)
+
+@app.route('/story')
+def show_madlib():
+    """Shows the Madlib story we just created from the homepage"""
+    return render_template('madlib.html')
